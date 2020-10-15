e3c019744edb0eaa15b562faa14cb177b8e23e91
Getu Gebre <getu.gebre.gg@gmail.com>
2020-10-15 09:29:17 -0500

    done

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+    @@all = []
+    attr_accessor :name, :age
+   def initialize (name, age)
+        @name = name
+        @age = age
+        save
+   end
+
+   def save
+        @@all << self
+   end
+
+   def new_meal (waiter, total, tip)
+        meal = Meal.new(waiter, self, total, tip)
+   end
+
+   def meals 
+        Meal.all.select {|meal| meal.customer == self}
+   end
+
+   def waiters
+        meals.map {|meal| meal.waiter}
+   end
+   def self.all
+        @@all
+   end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
 class Waiter
+    @@all = []
+    attr_accessor :name, :years
+   def initialize (name, years)
+        @name = name
+        @years = years
+        save
+   end
 
+   def save
+        @@all << self
+   end
+
+   def new_meal (customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals 
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+    #    select the meal with the largest tip
+    #  return the customer associated with this meal
+        meals.max {|meal1, meal2| meal1.tip <=> meal2.tip }.customer
+    end
+   def self.all
+        @@all
+   end
 end
